c28a9a5999a4fad23f7d8dcc57cda4a7d3fec75d
Vijayaa21 <134155725+Vijayaa21@users.noreply.github.com>
2024-01-03 22:52:18 +0530

    updated app and created email confirmation file and activation file for user

--- Psychologist/urls.py
+++ Psychologist/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import PasswordResetConfirmView
 
 urlpatterns = [
     path('', views.home, name = 'home'),
     path('signup', views.signup, name ='signup'),
     path('signin', views.signin, name ='signin'),
     path('signout', views.signout, name ='signout'),
+    path('activate/<slug:uidb64>/<slug:token>/', views.activate, name = "activate")
 ]
