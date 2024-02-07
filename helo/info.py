052d971da4ff8880b6493d3637f267c5d225f783
Vijaya Mishra <134155725+Vijayaa21@users.noreply.github.com>
2024-02-07 19:56:15 +0530

    Update info.py

--- helo/info.py
+++ helo/info.py
@@ -1,6 +1,6 @@
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'vijayam0421@gmail.com'
-EMAIL_HOST_PASSWORD = 'Ni$hu2104'
+EMAIL_HOST_USER = 'your-email@gmail.com'
+EMAIL_HOST_PASSWORD = 'password'
 EMAIL_PORT = 587
